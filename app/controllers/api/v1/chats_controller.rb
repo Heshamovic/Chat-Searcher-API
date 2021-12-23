168a0b9c8da2ad2b6e12a7aeb75e3d1c55047d77
Mohamed Hesham Othman <mohamed97198@gmail.com>
2021-12-23 21:49:56 +0200

    Fixing bug with viewing counted chat column and counted messages column

--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -8,10 +8,10 @@ module Api
         app = helper.get_app(params[:app_token])
         return render json: { status: 'ERROR', error: 'Application doesn\'t exist' }, status: :not_found if app.blank?
 
-        chat = helper.get_chat(app[0].id, params[:id])
+        chat = Chat.where(number: params[:id], app_id: app[0].id).first
         return render json: { status: 'ERROR', error: 'Chat doesn\'t exist' }, status: :not_found if chat.blank?
 
-        render json: { status: 'SUCCESS', chat: {number: chat[0].number, messages_count: chat[0].messages_count} }, status: :ok
+        render json: { status: 'SUCCESS', chat: {number: chat.number, messages_count: chat.messages_count} }, status: :ok
       end
 
       def create
@@ -26,11 +26,8 @@ module Api
           chat.app_id = app.id
           if chat.save
             app.update_column('chats_count', app.chats_count + 1)
-            ActiveRecord::Base.connection.execute('UNLOCK TABLES')
             return render json: { status: 'SUCCESS', message: 'Chat Created Successfully', chat_number: chat.number }, status: :created
           end
-
-          ActiveRecord::Base.connection.execute('UNLOCK TABLES')
           render json: { status: 'ERROR', message: 'Chat not Created', error: chat.errors }, status: :unprocessable_entity
         end
       end
--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -6,17 +6,16 @@ module Api
 
         return render json: { status: 'ERROR', error: 'Provide chat number' }, status: :unprocessable_entity if params[:chat_number].blank?
 
-        helper = Helper.new
-        app = helper.get_app(params[:app_token])
+        app = Application.where(token: params[:app_token]).first
         return render json: { status: 'ERROR', error: 'Application doesn\'t exist' }, status: :not_found if app.blank?
 
-        chat = helper.get_chat(app[0].id, params[:chat_number])
+        chat = Chat.where(number: params[:chat_number], app_id: app.id).first
         return render json: { status: 'ERROR', error: 'Chat doesn\'t exist' }, status: :not_found if chat.blank?
 
-        msg = helper.get_message(chat[0].id, params[:id])
+        msg = Message.where(chat_id: chat.id, number: params[:id]).first
         return render json: { status: 'ERROR', error: 'Message doesn\'t exist' }, status: :not_found if msg.blank?
 
-        render json: { status: 'SUCCESS', message: msg[0].body }, status: :ok
+        render json: { status: 'SUCCESS', message: msg.body }, status: :ok
       end
 
       def create
@@ -28,26 +27,22 @@ module Api
           return render json: { status: 'ERROR', error: 'Provide chat_number' }, status: :unprocessable_entity
         end
 
-        msg = Message.new
-        helper = Helper.new
-        app = helper.get_app(params[:app_token])
+        app = Application.where(token: params[:app_token]).first
         return render json: { status: 'ERROR', error: 'Application doesn\'t exist' }, status: :not_found if app.blank?
 
 
-        chat = Chat.where(number: params[:chat_number], app_id: app[0].id).first
+        chat = Chat.where(number: params[:chat_number], app_id: app.id).first
         return render json: { status: 'ERROR', error: 'Chat doesn\'t exist' }, status: :not_found if chat.blank?
 
         chat.with_lock do
-
+          msg = Message.new
           msg.number = chat.messages_count + 1
           msg.chat_id = chat.id
           msg.body = params[:body]
           if msg.save
             chat.update_column('messages_count', chat.messages_count + 1)
-            ActiveRecord::Base.connection.execute('UNLOCK TABLES')
             return render json: { status: 'SUCCESS', message: 'Message Created Successfully', message_number: msg.number }, status: :created
           end
-          ActiveRecord::Base.connection.execute('UNLOCK TABLES')
           render json: { status: 'ERROR', message: 'Message not Created', error: msg.errors }, status: :unprocessable_entity
         end
       end
@@ -56,6 +51,8 @@ module Api
         return render json: { status: 'ERROR', error: 'Provide app token' }, status: :unprocessable_entity if params[:app_token].blank?
 
         return render json: { status: 'ERROR', error: 'Provide chat number' }, status: :unprocessable_entity if params[:chat_number].blank?
+
+        return render json: { status: 'ERROR', error: 'Provide body' }, status: :unprocessable_entity if params[:body].blank?
 
         helper = Helper.new
         app = helper.get_app params[:app_token]
